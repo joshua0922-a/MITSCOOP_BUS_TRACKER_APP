8ed2a262f7e392e0b192b3142670275529370e48
MazeGuy <joshuapatriarcaqqww@gmail.com>
2026-03-09 18:49:52 +0800

    Add server implementation and Vercel configuration for deployment

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const api = ""; // leave blank if same server
+const api = "https://mitscoop-backend.vercel.app"; // leave blank if same server
 
 /* =============================== LOAD BUSES =============================== */
 async function loadBuses() {
